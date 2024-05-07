cbf5429fed67f56beddb4f2e295b9deb13bf0b10
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-05-07 17:44:48 -0500

    feat(components): initial commit

--- apps/web-design-system/src/app/app.tsx
+++ apps/web-design-system/src/app/app.tsx
@@ -3,10 +3,13 @@ import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
+import { IconButton } from '@bsahitya/react-components';
+
 export function App() {
   return (
     <div>
       <NxWelcome title="web-design-system" />
+      <IconButton icon="home" />
     </div>
   );
 }

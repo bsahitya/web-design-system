da211d0383f281c7097b532e83df09e779884250
Sahitya Buddharaju <sahitya.buddharaju@gmail.com>
2024-05-08 11:54:03 -0500

    ci(storybook): deploy storybook to github pages

--- libs/react-components/.storybook/main.ts
+++ libs/react-components/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {
       builder: {
-        viteConfigPath: 'vite.config.ts',
+        viteConfigPath: 'libs/react-components/vite.config.ts',
       },
     },
   },
